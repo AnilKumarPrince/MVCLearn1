d75e067fb43c4452a6cfbe6a458470b6f5e4528b
AnilKumarPrince <anil38402@gmail.com>
2023-01-17 09:41:03 +0530

    My First CheckIn

---
+++ MVCLearn1/MVCLearn1/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLearn1.Models;
+
+namespace MVCLearn1.Controllers
+{
+	public class LoginController : Controller
+	{
+        ApplicationDBContext1 db;
+        User obj;
+        public LoginController(ApplicationDBContext1 _db)
+        {
+            obj= new User();
+            db = _db;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+        public IActionResult Register(User ObjUser)
+        {
+            db.Users.Add(ObjUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCLearn1/MVCLearn1/Models/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCLearn1.Models
+{
+	public class ApplicationDBContext1 : DbContext
+	{
+		
+		public ApplicationDBContext1(DbContextOptions<ApplicationDBContext1> options) : base(options)
+		{
+
+		}
+
+		public DbSet<User> Users { get; set; }
+	}
+}
