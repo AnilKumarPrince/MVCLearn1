a7d239ccd3e70578b26e96534412231f600fa9c0
AnilKumarPrince <anil38402@gmail.com>
2023-01-25 09:19:55 +0530

    New File Added

--- MVCLearn1/MVCLearn1/Controllers/LoginController.cs
+++ MVCLearn1/MVCLearn1/Controllers/LoginController.cs
@@ -14,13 +14,102 @@ namespace MVCLearn1.Controllers
         }
         public IActionResult Index()
 		{
-			return View();
+            var lst = db.Users.ToList();
+            List<User> lstUser = new List<User>();
+            //User user= new User();
+            //user.Name = "Anil";
+            //user.Email = "anil39@gmail.com";
+            //user.Password = "9867";
+            //lstUser.Add(user);
+			return View(lst);
 		}
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
         public IActionResult Register(User ObjUser)
         {
             db.Users.Add(ObjUser);
             db.SaveChanges();
             return RedirectToAction("Index");
+            //return View();
         }
+        public IActionResult Edit(int Id)
+        {
+            var obj = db.Users.FirstOrDefault(x => x.Id == Id);
+            if (Id == null)
+            {
+                
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Edit(User Objuser)
+        {
+            if(ModelState.IsValid )
+            { 
+           
+            var founduser = db.Users.Find(Objuser.Id);
+            if (founduser != null)
+            {
+                founduser.Name = Objuser.Name;
+                founduser.Email = Objuser.Email;
+                founduser.Password = Objuser.Password;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            }
+            return View();           
+        }
+
+
+        public IActionResult Delete(int Id)
+        {
+            var founduser = db.Users.Find(Id);
+            db.Users.Remove(founduser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+            //if (Id == null)
+            //{
+
+            //    return NotFound();
+            //}
+            //var founduser = db.Users.FirstOrDefault(x => x.Id == Id);
+            //return View(founduser);
+        }
+        //[HttpPost]
+        //public IActionResult Delete(int Id)
+        //{
+        //    var founduser = db.Users.Find(Id);
+        //    if (founduser == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    db.Users.Remove(founduser);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+
+        //}
+        [HttpPost]
+        public IActionResult Deletepost(int Id)
+        {
+            //if (ModelState.IsValid)
+            //{
+            var founduser = db.Users.Find(Id);
+            db.Users.Remove(founduser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            //}
+
+            //return RedirectToAction("Register");
+
+        }
+
+
     }
 }
